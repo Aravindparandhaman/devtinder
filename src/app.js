d2ce61fdba934c544d63157993d2107c5009883f
Aravind <163089676+Aravindparandhaman@users.noreply.github.com>
2026-02-27 12:33:38 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const app = express();
 
-
-
-app.get("/home", (req,res) =>{
-    console.log("Home screen");
-    res.send("Getting Home Screen")
+app.get("/user", (req, res) =>{
+    res.send({firstNname:"Aravind", lastName:"Dheena" })
 })
+
+app.post("/user", (req,res) =>{
+    res.send("Data successfully in DatBase")
+    console.log("Data successfully in DatBas")
+})
+
+app.delete("/delete", (req,res) =>{
+    res.send("Data deleted Successfully")
+    console.log("Data deleted successfully")
+})
+
 
 app.use("/", (req, res) =>{
     res.send("slash screen")
     console.log("Splash screen is runing successfully")
 })
+
+
+
+
 
 
 
